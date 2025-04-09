dab1f52005e9ebfdf2f837f1f597e433c29ff95b
lelety2007 <leticiagrazielle258@gmail.com>
2025-04-09 16:55:58 +0000

    concluida

---
+++ objetos.js
@@ -0,0 +1,11 @@
+const endereco = {
+    logradouro: " Rua Francisco bastos Magalhaes",
+    cep: "46500000",
+    estado: "bahia",
+    municipio: "macaubas"
+} ;
+
+console.log(endereco);
+console.log(cep);
+console.log(estado);
+console.log(municipio);
